abe860c5e87dce63889b6ec8e19b73d702225398
Benjamin Besse <benjamin.besse@aquassay.com>
2022-07-11 15:25:46 +0200

    add getURL method

--- src/index.js
+++ src/index.js
@@ -192,4 +192,19 @@ const horizon = (series, {
     return svg.node();
 };
 
+const getURL = (svgElement) => {
+    const svg           = svgElement.cloneNode(true);
+    const serializer    = new XMLSerializer();
+    const rules         = svg.querySelectorAll('.rule');
+
+    rules.forEach(rule => rule.remove());
+
+    const source = serializer.serializeToString(svg);
+    const url    = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(source)}`;
+
+    return url;
+};
+
+export { getURL };
+
 export default horizon;
